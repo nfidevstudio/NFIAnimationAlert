cc9e433ec9537aac7d5d42eb50ce8f0c0bf4d880
José Carlos <josecarlosestela@gmail.com>
2016-02-10 10:01:25 +0100

    Initial Commit

---
+++ ExampleAnimationAlert/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ExampleAnimationAlert
+//
+//  Created by José Carlos on 10/2/16.
+//  Copyright © 2016 José Carlos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ExampleAnimationAlert/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ExampleAnimationAlert
+//
+//  Created by José Carlos on 10/2/16.
+//  Copyright © 2016 José Carlos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
